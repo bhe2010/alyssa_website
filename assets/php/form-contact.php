ac0ecdbc69ecf2f6f8c3245441b14d3ab6f8f04a
Benjamin He <bhevball@gmail.com>
2023-08-22 12:58:00 -0500

    simplified contact form

--- assets/php/form-contact.php
+++ assets/php/form-contact.php
@@ -80,8 +80,8 @@ $mail = new PHPMailer(true);
 $mail->isSMTP();
 $mail->Host        = 'smtp.gmail.com';
 $mail->SMTPAuth    = true;
-$mail->Username    = 'bhevball@gmail.com'
-$mail->Password    = 'xljbbnebatodhybi';
+$mail->Username    = getenv('UN');
+$mail->Password    = getenv('PW');
 $mail->SMTPSecure  = 'tls';
 $mail->Port        = 587;
 
